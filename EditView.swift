96aeb12bbe30b8bb183abceba1607ff9affd9be2
Juan Galeazzi <jgaleazzi@gmail.com>
2026-09-23 14:29:49 -0500

    added EditView

---
+++ EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Juan Galeazzi on 9/23/26.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
