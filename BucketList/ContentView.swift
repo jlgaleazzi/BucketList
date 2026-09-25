ea76e93ffd3e2ec5d5340f4d5630540918a57187
Juan Galeazzi <jgaleazzi@gmail.com>
2026-09-25 13:29:17 -0500

    addee wikipedia neaby places

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -73,22 +73,26 @@ struct ContentView: View {
                                     .frame(width:44, height: 44)
                                     .background(.white)
                                     .clipShape(.circle)
-                                    .onLongPressGesture {
+                                    .onLongPressGesture(minimumDuration: 0.2) {
                                         selectedPlace = location
                                     }
+                                }
                             }
                         }
-                    }
-                    .sheet(item: $selectedPlace) {
-                        place in Text(place.name)
-                    }
-                            .onTapGesture {
-                            position in
+                        .onTapGesture { position in
                             if let coordinate = proxy.convert(position, from: .local) {
                                 let newLocation = Location(id:UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
                                 locations.append(newLocation)
+                                selectedPlace = newLocation
                             }
                         }
+                            .sheet(item: $selectedPlace) { place in
+                                EditView(location: place) { newLocation in
+                                    if let index = locations.firstIndex(of: place) {
+                                        locations[index] = newLocation
+                                    }
+                                }
+                            }
                            
                             
         
